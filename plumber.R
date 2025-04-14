2d305eef3c654221919c06398646693840b12abd
Pierrick Kinif <pierrick-kinif@hotmail.be>
2025-04-14 20:54:41 +0200

    add_bearer_authentication linked to postgres

--- plumber.R
+++ plumber.R
@@ -7,6 +7,21 @@ library(RPostgres)
 library(glue)
 library(luxJob)
 
+auth_helper <- function(res, req, FUN, ...) {
+    # Get token from header
+    auth_header <- if (!is.null(req$HTTP_AUTHORIZATION)) req$HTTP_AUTHORIZATION else ""
+    token <- sub("^Bearer ", "", auth_header)
+    valid_token <- luxJob::verify_token(token, schema = Sys.getenv("sql_schema")) 
+    
+    if (!valid_token) {
+        res$status <- 401
+        return(list(error = "Unauthorized: Invalid or missing token"))
+    }
+    
+    FUN(...)
+}
+
+
 # ------------------------------------------------------------------------------
 # Skills
 # ------------------------------------------------------------------------------
@@ -14,16 +29,20 @@ library(luxJob)
 #* Get all skills
 #* @get /skills
 #* @param limit:int Maximum number of skills to return
-function(limit = 100) {
-    luxJob::get_skills(as.integer(limit))
+function(res, req, limit = 100) {
+    auth_helper(res, req, function(limit) {
+        luxJob::get_skills(as.integer(limit))
+    }, limit = limit)
 }
 
 #* Get a skill by ID
 #* @get /skills/<skill_id>
 #* @param skill_id:string ID of the skill to retrieve
-function(skill_id = 'http://data.europa.eu/esco/skill/97965983-0da4-4902-9daf-d5cd2693ef73') {
-    skill_id <- utils::URLdecode(skill_id)
-    luxJob::get_skill_by_id(as.character(skill_id))
+function(res, req, skill_id = 'http://data.europa.eu/esco/skill/97965983-0da4-4902-9daf-d5cd2693ef73') {
+    auth_helper(res, req, function(skill_id) {
+        skill_id <- utils::URLdecode(skill_id)
+        luxJob::get_skill_by_id(as.character(skill_id))
+    }, skill_id = skill_id)
 }
 
 # ------------------------------------------------------------------------------
@@ -33,15 +52,19 @@ function(skill_id = 'http://data.europa.eu/esco/skill/97965983-0da4-4902-9daf-d5
 #* Get all companies
 #* @get /companies
 #* @param limit:int Maximum number of companies to return
-function(limit = 100) {
-    luxJob::get_companies(as.integer(limit))
+function(res, req, limit = 100) {
+    auth_helper(res, req, function(limit) {
+        luxJob::get_companies(as.integer(limit))
+    }, limit = limit)
 }
 
 #* Get a company by ID
 #* @get /companies/<company_id>
 #* @param company_id:int ID of the company to retrieve
-function(company_id) {
-    luxJob::get_company_details(as.integer(company_id))
+function(res, req, company_id) {
+    auth_helper(res, req, function(company_id) {
+        luxJob::get_company_details(as.integer(company_id))
+    }, company_id = company_id)
 }
 
 # ------------------------------------------------------------------------------
@@ -54,20 +77,24 @@ function(company_id) {
 #* @param company:int Company ID to filter by
 #* @param canton:string Canton name to filter by
 #* @param limit:int Maximum number of results to return
-function(skill = NULL, company = NULL, canton = NULL, limit = 100) {
-    luxJob::get_vacancies(
-        skill = skill,
-        company = if (!is.null(company)) as.integer(company) else NULL,
-        canton = canton,
-        limit = as.integer(limit)
-    )
+function(res, req, skill = NULL, company = NULL, canton = NULL, limit = 100) {
+    auth_helper(res, req, function(skill, company, canton, limit) {
+        luxJob::get_vacancies(
+            skill = skill,
+            company = if (!is.null(company)) as.integer(company) else NULL,
+            canton = canton,
+            limit = as.integer(limit)
+        )
+    }, skill = skill, company = company, canton = canton, limit = limit)
 }
 
 #* Get a vacancy by ID
 #* @get /vacancies/<vacancy_id>
 #* @param vacancy_id:int ID of the vacancy to retrieve
-function(vacancy_id) {
-    luxJob::get_vacancy_by_id(as.numeric(vacancy_id))
+function(res, req, vacancy_id) {
+    auth_helper(res, req, function(vacancy_id) {
+        luxJob::get_vacancy_by_id(as.numeric(vacancy_id))
+    }, vacancy_id = vacancy_id)
 }
 
 # ------------------------------------------------------------------------------
@@ -77,15 +104,19 @@ function(vacancy_id) {
 #* Get learning tracks
 #* @get /learning_tracks
 #* @param skill_id:string Optional skill ID to filter learning tracks
-function(skill_id = NULL) {
-    luxJob::get_learning_tracks(skill_id)
+function(res, req, skill_id = NULL) {
+    auth_helper(res, req, function(skill_id) {
+        luxJob::get_learning_tracks(skill_id)
+    }, skill_id = skill_id)
 }
 
 #* Get a learning track by ID
 #* @get /learning_tracks/<track_id>
 #* @param track_id:int ID of the learning track to retrieve
-function(track_id) {
-    luxJob::get_learning_track_by_id(as.integer(track_id))
+function(res, req, track_id) {
+    auth_helper(res, req, function(track_id) {
+        luxJob::get_learning_track_by_id(as.integer(track_id))
+    }, track_id = track_id)
 }
 
 # ------------------------------------------------------------------------------
@@ -95,15 +126,19 @@ function(track_id) {
 #* Get book recommendations
 #* @get /books
 #* @param skill:string Optional skill ID to filter books
-function(skill = 'http://data.europa.eu/esco/skill/70198e4e-86ad-4acc-a9eb-e24e2c107d18') {
-    luxJob::get_books(skill)
+function(res, req, skill = 'http://data.europa.eu/esco/skill/70198e4e-86ad-4acc-a9eb-e24e2c107d18') {
+    auth_helper(res, req, function(skill) {
+        luxJob::get_books(skill)
+    }, skill = skill)
 }
 
 #* Get a book by ID
 #* @get /books/<book_id>
 #* @param book_id:int ID of the book to retrieve
-function(book_id) {
-    luxJob::get_book_by_id(as.integer(book_id))
+function(res, req, book_id) {
+    auth_helper(res, req, function(book_id) {
+        luxJob::get_book_by_id(as.integer(book_id))
+    }, book_id = book_id)
 }
 
 # ------------------------------------------------------------------------------
@@ -114,6 +149,8 @@ function(book_id) {
 #* @post /log_search
 #* @param user_id:int ID of the user making the search
 #* @param query:string Text of the search query
-function(user_id, query) {
-    luxJob::log_search(as.integer(user_id), query)
+function(res, req, user_id, query) {
+    auth_helper(res, req, function(user_id, query) {
+        luxJob::log_search(as.integer(user_id), query)
+    }, user_id = user_id, query = query)
 }
--- run_plumber.R
+++ run_plumber.R
@@ -1,3 +1,45 @@
+add_bearer_auth <- function(api, paths = NULL) {
+  api$components <- list(
+    securitySchemes = list(
+      BearerAuth = list(
+        type = "http",
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        description = "Enter a Bearer token like: Bearer abc123"
+      )
+    )
+  )
+  
+  if (is.null(paths)) paths <- names(api$paths)
+  
+  for (path in paths) {
+    methods <- names(api$paths[[path]])
+    for (method in intersect(methods, c("get", "post", "put", "delete", "head"))) {
+      api$paths[[path]][[method]] <- c(
+        api$paths[[path]][[method]],
+        list(security = list(list(BearerAuth = list())))
+      )
+    }
+  }
+  
+  api
+}
+
 
 plumber::pr("plumber.R") |> 
-  plumber::pr_run(port = 8008)
+  plumber::pr_set_api_spec(add_bearer_auth) |>
+  plumber::pr_hook("preroute", function(req, res) {
+    res$setHeader("Access-Control-Allow-Origin", "http://localhost:1234")
+    res$setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+    res$setHeader("Access-Control-Allow-Headers", "API_KEY, Accept")
+    res$setHeader("Access-Control-Allow-Credentials", "true")
+    if (req$REQUEST_METHOD == "OPTIONS") {
+      res$status <- 200
+      return(list())
+    }
+    plumber::forward()
+  }) |>
+  plumber::pr_run(
+    port = 8008,
+    host = "0.0.0.0"
+  )
